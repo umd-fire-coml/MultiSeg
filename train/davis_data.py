bcdd6c7fa83f02e1e9986d490a691a276c93527a
kjhurley99 <32457070+kjhurley99@users.noreply.github.com>
2018-07-24 15:32:36 -0400

    Update davis_data.py

--- train/davis_data.py
+++ train/davis_data.py
@@ -258,7 +258,7 @@ class DAVISDataset(utils.Dataset):
         if len(raw_mask.shape) == 3:
            raw_mask = raw_mask[...,0]
         if image.shape[-1] == 3:
-            image = image[..., :0]
+           raw_mask = raw_mask[..., :0]
         raw_mask = raw_mask.reshape(self.image_height, self.image_width, 1)
 
         # broadcast!!!!
