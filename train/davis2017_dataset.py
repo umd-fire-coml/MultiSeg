bede634f5c7c087329ab86baee1ea1815a2db56a
Timothy Lin <tmthylin@gmail.com>
2018-08-25 21:18:49 -0400

    continued work on Davis 2017 dataset functionality (and general dataset refactoring)

--- train/davis2017_dataset.py
+++ train/davis2017_dataset.py
@@ -1,12 +1,54 @@
+import os
 from os import path
 
 from image_seg import utils
 
 
-class Davis2017Dataset(utils.Dataset):
+__all__ = ['Davis2017Dataset',
+           'generate_datasets_from_videos',
+           'generate_train_val_datasets',
+           'generate_full_dataset']
 
-    def load_data(self, subset: str, quality: str):
+_subset_param_desc = "which subset of DAVIS 2017 to get (either 'trainval', " \
+                      "'test-dev', or 'test-challenge')"
+
+_quality_param_desc = "quality of the video (either '480p', '1080p', or 'fullres'); " \
+                       "'1080p' and 'fullres' refer to the same image quality"
+
+_selection_param_desc = "which objects to retrieve (either 'images', 'labels', or " \
+                         "'videos')"
+
+
+class Davis2017Dataset(utils.Dataset):
+    name = 'DAVIS2017'
+    def __init__(self, subset, quality, data_dir='./'):
+        super(self.__class__, self).__init__(self)
+
+        self.subset = subset
+        self.quality = quality
+        self.data_dir = data_dir
+
+    def load_all_data(self):
         pass
+
+    def load_data(self, video: str, frame_ids, assume_match=True):
+        try:
+            iter(frame_ids)
+        except TypeError:
+            frame_ids = list(frame_ids)
+
+        image_path = path.join(self.data_dir,
+                               Davis2017Dataset.build_relative_path(self.subset, self.quality, 'images'),
+                               video)
+        label_path = path.join(self.data_dir,
+                               Davis2017Dataset.build_relative_path(self.subset, self.quality, 'labels'),
+                               video)
+
+        for fid in frame_ids:
+            img_path = path.join(image_path, f'{fid}.jpg')
+            lab_path = path.join(image_path, f'{fid}.png')
+
+            self.add_image(self.name, f'{video}{fid}', img_path, mask_path=lab_path)
 
     def load_image(self, image_id):
         pass
@@ -14,8 +56,19 @@ class Davis2017Dataset(utils.Dataset):
     def load_mask(self, image_id):
         pass
 
+    def __str__(self):
+        return '<Davis 2017 Dataset>'
+
     @staticmethod
-    def _build_relative_path(subset: str, quality: str, selection: str):
+    def build_relative_path(subset: str, quality: str, selection: str):
+        f"""
+        Builds a relative path to the directory containing the dataset with the
+        parameters given.
+        :param subset: {_subset_param_desc}
+        :param quality: {_quality_param_desc}
+        :param selection: {_selection_param_desc}
+        :return: path to the desired selection
+        """
         if subset not in ['trainval', 'test-dev', 'test-challenge']:
             raise ValueError(f'{subset} not a subset for DAVIS 2017')
         if quality not in ['480p', 'fullres', '1080p']:
@@ -40,11 +93,47 @@ class Davis2017Dataset(utils.Dataset):
         return path.join(base_path, rel_path)
 
 
-def generate_datasets_from_videos(subset: str, quality: str) -> list:
-    pass
+def generate_datasets_from_videos(root_dir: str, subset: str, quality: str) -> list:
+    images_root_dir = path.join(root_dir, Davis2017Dataset.build_relative_path(subset, quality, 'images'))
+    labels_root_dir = path.join(root_dir, Davis2017Dataset.build_relative_path(subset, quality, 'labels'))
+
+    _, videos_from_images, _ = next(os.walk(images_root_dir))
+    _, videos_from_labels, _ = next(os.walk(labels_root_dir))
+    videos_from_images = set(videos_from_images)
+    videos_from_labels = set(videos_from_labels)
+
+    videos = videos_from_images & videos_from_labels
+
+    datasets = []
+
+    for video_filename in videos:
+        new_dataset = Davis2017Dataset()
+
+        image_dir = path.join(images_root_dir, video_filename)
+        label_dir = path.join(labels_root_dir, video_filename)
+
+        _, _, images = next(os.walk(image_dir))
+        _, _, labels = next(os.walk(label_dir))
+        images = set(images)
+        labels = set(labels)
+
+        print('from images', images)
+        print('from labels', labels)
+
+        images = images & labels
+
+        datasets.append(new_dataset)
+
+    return datasets
 
 
 def generate_train_val_datasets(subset: str, quality: str) -> (Davis2017Dataset, Davis2017Dataset):
+    f"""
+    Generates the default DAVIS2017 train and validation datasets.
+    :param subset: {_subset_param_desc}
+    :param quality: {_quality_param_desc}
+    :return: (train dataset, val dataset)
+    """
     pass
 
 
@@ -53,4 +142,9 @@ def generate_full_dataset(subset: str, quality: str) -> Davis2017Dataset:
     dataset.load_data(subset, quality)
 
     return dataset
+
+
+if __name__ == '__main__':
+    lst = generate_datasets_from_videos('G:\Team Drives\COML-Summer-2018\Data\DAVIS', 'trainval', '480p')
+    print(lst)
 
