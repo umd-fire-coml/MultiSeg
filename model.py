4dbfb6a16bf4fc600c57c8751339f8c5c9db389e
Timothy Lin <tmthylin@gmail.com>
2018-11-05 20:24:21 -0500

    started multiseg object

--- model.py
+++ model.py
@@ -1,2 +1,24 @@
 # TODO make integrated model here
+import keras.models as km
+import keras.layers as kl
 
+
+class MultiSeg(object):
+    
+    def __init__(self, mode: str):
+        if mode not in ['train', 'infer']:
+            raise ValueError('MultiSeg mode must either be \'train\' or \'infer\'')
+        
+        self._mode = mode
+        self._model = self._build_model()
+        
+    def _build_model(self) -> km.Model:
+        
+        prev_image = kl.Input((None, None, 3), )
+        curr_image = input
+        
+        return km.Model(inputs=[], outputs=[])
+
+    @property
+    def mode(self):
+        return self._mode
